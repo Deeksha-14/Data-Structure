e9ae146fa919a2d6db0bca021d92610f74b2b72b
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-07-30 00:15:31 +0530

    Create PrintArray.java

---
+++ Sorting/InsertionSort/PrintArray.java
@@ -0,0 +1,14 @@
+package InsertionSort;
+
+public class PrintArray {
+	
+	public static void printArray(int[] arr)
+	{
+		for(int n: arr)
+		{
+			System.out.print(n+" ");
+		}
+		System.out.println(" ");
+	}
+
+}
