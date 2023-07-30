f936936b9ef4568df1ad72d367c242435cb8c045
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-07-30 23:08:35 +0530

    Create PrintArray.java

---
+++ Sorting/HeapSort/PrintArray.java
@@ -0,0 +1,14 @@
+package HeapSort;
+
+public class PrintArray {
+	
+	public static void printArray(int[] arr)
+	{
+		for(int num:arr)
+		{
+			System.out.print(num+" ");
+		}
+	System.out.println();
+	}
+
+}
